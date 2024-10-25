eb7576c8ee60e307310917d92ca81295a828b79d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-25 22:23:56 +0300

    Create User.cs

---
+++ Photography/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Photography.Data
+{
+    public class User : IdentityUser
+    {
+        //public User()
+        //{
+        //    this.Id = Guid.NewGuid();
+        //}
+
+        public string Id { get; set; } = null!;
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+
+
+
+    }
+}
