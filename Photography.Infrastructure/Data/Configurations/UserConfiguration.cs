2747b1c58192aa81f906f49f748928a6460c15cf
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-10-27 14:01:10 +0200

    Create UserConfiguration.cs

---
+++ Photography.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Photography.Infrastructure.Data.Models;
+
+namespace Photography.Infrastructure.Data.Configurations
+{
+    public class UserConfiguration:IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.JoinedAt)
+                .HasDefaultValue(DateTime.Now);
+        }
+    }
+}
