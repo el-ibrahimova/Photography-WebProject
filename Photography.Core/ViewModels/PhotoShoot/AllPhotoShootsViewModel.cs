7df4defd61f484041ed94c3927afb76fa76402fa
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-26 23:46:34 +0200

    Create AllPhotoShootsViewModel.cs

---
+++ Photography.Core/ViewModels/PhotoShoot/AllPhotoShootsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Photography.Infrastructure.Data.Models;
+
+namespace Photography.Core.ViewModels.PhotoShoot
+{
+    public class AllPhotoShootsViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl1 { get; set; } = null!;
+        public string? ImageUrl2 { get; set;} 
+        public string? ImageUrl3 { get; set;}
+        public string CreatedAt { get; set; } = null!;
+    }
+}
