b0b33d37fa7620a5c6702f78ee475a4b2cafd2cf
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-29 23:00:56 +0200

    create bool IsAdmin

--- Photography/Extensions/ClaimsPrincipalExtensions.cs
+++ Photography/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using static Photography.Common.ApplicationConstants;
 
 namespace Photography.Extensions
 {
@@ -7,6 +8,11 @@ namespace Photography.Extensions
         public static string? GetUserId(this ClaimsPrincipal? userClaimsPrincipal)
         {
             return userClaimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
         }
 
     }
