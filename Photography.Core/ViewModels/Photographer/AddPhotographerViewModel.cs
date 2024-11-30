7b3fbb083a6defed740f87d2f66351a82bbcdcb5
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-30 13:47:58 +0200

    add view models for Photographer

---
+++ Photography.Core/ViewModels/Photographer/AddPhotographerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Photography.Common.EntityValidationMessages;
+using static Photography.Common.EntityConstants.Photographer;
+
+namespace Photography.Core.ViewModels.Photographer
+{
+    public class AddPhotographerViewModel
+    {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(BrandNameMaxLength, MinimumLength = BrandNameMinLength, ErrorMessage = LengthMessage)]
+        [Display(Name ="Име на марката")]
+        public string BrandName { get; set; } = null!;
+    }
+}
---
+++ Photography.Core/ViewModels/Photographer/PhotographerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Photography.Core.ViewModels.Photographer
+{
+    public class PhotographerViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string BrandName { get; set; } = null!;
+    }
+}
