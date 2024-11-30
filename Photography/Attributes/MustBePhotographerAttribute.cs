1627ed5dbd8a28828d5fa9e742afea800c39ae45
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-30 13:38:41 +0200

    create class attribute for checking whether user is photographer or not

---
+++ Photography/Attributes/MustBePhotographerAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Photography.Controllers;
+using Photography.Core.Interfaces;
+using Photography.Extensions;
+
+namespace Photography.Attributes
+{
+    public class MustBePhotographerAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IPhotographerService? photographerService = context.HttpContext.RequestServices.GetService<IPhotographerService>();
+
+            if (photographerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (photographerService != null
+                && photographerService.ExistsByIdAsync(context.HttpContext.User.GetUserId()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(PhotographerController.Add), "Photographer", null);
+            }
+        }
+    }
+}
