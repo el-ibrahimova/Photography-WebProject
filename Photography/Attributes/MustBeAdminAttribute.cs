d0ea854431c5139991b7c32bcd136ba10401338a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-30 17:44:57 +0200

    Create MustBeAdminAttribute.cs

---
+++ Photography/Attributes/MustBeAdminAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Photography.Controllers;
+using Photography.Core.Interfaces;
+using Photography.Extensions;
+
+namespace Photography.Attributes
+{
+    public class MustBeAdminAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IUserService? userService = context.HttpContext.RequestServices.GetService<IUserService>();
+
+            if (userService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (userService != null
+                && userService.UserExistByIdAsync(Guid.Parse(context.HttpContext.User.GetUserId())).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(GalleryController.Gallery), "Gallery", null);
+            }
+        }
+    }
+}
