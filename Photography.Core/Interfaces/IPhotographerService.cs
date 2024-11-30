89233d3087c2a7e72dd223dd202920304db79df2
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-30 13:47:40 +0200

    create PhotographerService and its interface

---
+++ Photography.Core/Interfaces/IPhotographerService.cs
@@ -0,0 +1,10 @@
+﻿namespace Photography.Core.Interfaces
+{
+    public interface IPhotographerService
+    {
+        Task<bool> ExistsByIdAsync(string userId);
+        Task CreateAsync(string userId, string brandName);
+
+        Task<bool> UserWithBrandNameExistAsync(string brandName);
+    }
+}
---
+++ Photography.Core/Services/PhotographerService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Photography.Core.Interfaces;
+using Photography.Infrastructure.Data;
+using Photography.Infrastructure.Data.Models;
+
+namespace Photography.Core.Services
+{
+    public class PhotographerService:BaseService,IPhotographerService
+    {
+
+        private readonly PhotographyDbContext context;
+
+        public PhotographerService(PhotographyDbContext data)
+            : base(data)
+        {
+            context = data;
+        }
+
+        public async Task<bool> ExistsByIdAsync(string userId)
+        {
+            return await context.Photographers
+                .AnyAsync(p=>p.UserId.ToString().ToLower()== userId.ToLower());
+        }
+
+        public async Task CreateAsync(string userId, string brandName)
+        {
+            await context.AddAsync(new Photographer()
+            {
+                UserId = Guid.Parse(userId),
+                BrandName = brandName
+            });
+        }
+
+        public async Task<bool> UserWithBrandNameExistAsync(string brandName)
+        {
+          return  await context.Photographers.AnyAsync(p => p.BrandName == brandName);
+        }
+    }
+}
