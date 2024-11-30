ecbcd6408c55f22382bab0570bb6180971067da8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-11-30 13:39:06 +0200

    Update EntityValidationMessages.cs

--- Photography.Common/EntityValidationMessages.cs
+++ Photography.Common/EntityValidationMessages.cs
@@ -2,25 +2,9 @@
 {
     public static class EntityValidationMessages
     {
-        public static class Photo
-        {
-            public const string PhotoUploadedAtRequiredMessage = "Датата на качване на снимката е задължителна.";
-            public const string PhotoImageUrlRequiredMessage = "URL адрес на снимката е задължителен.";
-        }
+        public const string LengthMessage = "Полето {0} трябва да съдържа от {2} до {1} символа";
+        public const string RequiredMessage = "Полето {0} е задължително";
 
-        public static class Category
-        {
-            public const string CategoryNameRequiredMessage = "Името на категорията е задължително.";
-        }
-
-        public static class PhotoShoot
-        {
-            public const string PhotoShootNameRequiredMessage = "Името на фотосесията е задължително.";
-            public const string PhotoShootImageUrlRequiredMessage = "URL адрес на снимката е задължително.";
-            public const string PhotoShootDescriptionRequiredMessage = "Описанието на фотосесията е задължително.";
-
-            public const string PhotoShootCreatedAtRequiredMessage =
-                "Датата на създаване на фотосесията е задължителна.";
-        }
+        public const string BrandNameExist = "Тази марка вече съществува";
     }
 }
