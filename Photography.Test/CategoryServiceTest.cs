28e22268099a65874f0c229e290e545e3b604970
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-12-11 11:29:33 +0200

    create Test unit

---
+++ Photography.Test/CategoryServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Photography.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
